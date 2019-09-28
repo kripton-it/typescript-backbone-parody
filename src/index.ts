87f7d07ba6b9cf2da573bb1f695ce04398e7ff96
Mikhail Shardin <kriptonit1988@gmail.com>
2019-09-28 19:00:59 +0500

    Обращение к методам низкоуровневых классов с помощью геттеров

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,3 @@
 import { User } from "./models/User";
 
 const user = new User({ name: "newName" });
-
-user.set({ age: 88 });
-
-user.save();
-/*const user = new User({ name: "John", age: 50 });
-
-user.on("change", () => {
-  console.log(1);
-});
-
-user.on("change", () => {
-  console.log(2);
-});
-
-user.on("click", () => {
-  console.log(3);
-});
-
-user.trigger("change");
- */
--- src/models/Eventing.ts
+++ src/models/Eventing.ts
@@ -1,15 +1,15 @@
-type Callback = () => void;
+export type Callback = () => void;
 
 export class Eventing {
   events: { [key: string]: Callback[] } = {};
-  
-  on(eventName: string, callback: Callback): void {
+
+  on = (eventName: string, callback: Callback): void => {
     const callbacks = this.events[eventName] || [];
     callbacks.push(callback);
     this.events[eventName] = callbacks;
   }
 
-  trigger(eventName: string): void {
+  trigger = (eventName: string): void => {
     const callbacks = this.events[eventName] || [];
     callbacks.forEach((callback: Callback): void => {
       callback();
